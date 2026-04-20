e3fb949a00c61d9188d4e73c53ef08639990a251
Rehan Shafique <rehanshafiq6540@gmail.com>
2026-04-20 14:09:30 +0500

    Refactor model loading with Streamlit caching

--- model_loader.py
+++ model_loader.py
@@ -1,18 +1,16 @@
 import os
 import gdown
+import streamlit as st
 from tensorflow.keras.models import load_model
 
-MODEL_PATH = "skin_cancer_model.h5"
+MODEL="skin_cancer_model.h5"
+FILE_ID="18VE_D81425cZVYwAXjOn0gWti8_lZSML"
 
-FILE_ID = "18VE_D81425cZVYwAXjOn0gWti8_lZSML"
+@st.cache_resource
+def load_model_safe():
 
-def get_model():
+    if not os.path.exists(MODEL):
+        url=f"https://drive.google.com/uc?export=download&id={FILE_ID}"
+        gdown.download(url,MODEL,quiet=False)
 
-    if not os.path.exists(MODEL_PATH):
-        print("Downloading AI model...")
-
-        url = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
-        gdown.download(url, MODEL_PATH, quiet=False)
-
-    model = load_model(MODEL_PATH)
-    return model
+    return load_model(MODEL)
