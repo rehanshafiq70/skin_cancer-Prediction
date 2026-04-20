d8816369a0bdd4c7e604110ea18d95894a804e4d
Rehan Shafique <rehanshafiq6540@gmail.com>
2026-04-20 13:39:53 +0500

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -3,11 +3,14 @@ import gdown
 from tensorflow.keras.models import load_model
 
 MODEL_PATH = "skin_cancer_model.h5"
+
 FILE_ID = "18VE_D81425cZVYwAXjOn0gWti8_lZSML"
 
-def load_ai_model():
+def get_model():
 
     if not os.path.exists(MODEL_PATH):
+        print("Downloading AI model...")
+
         url = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
         gdown.download(url, MODEL_PATH, quiet=False)
 
