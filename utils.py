56844bed3eb2f8d615fc47af52eec8bea377089a
Rehan Shafique <rehanshafiq6540@gmail.com>
2026-04-20 14:12:50 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,22 +1,27 @@
 import numpy as np
 
-def preprocess(img):
-
+def preprocess_image(img):
     img = img.resize((224,224))
-    img = np.array(img)/255.0
+    arr = np.array(img)/255.0
+    return np.expand_dims(arr,0)
 
-    if img.std() < 0.02:
-        return None
+def validate_image(img):
+    arr=np.array(img)
+    return arr.std()>5
 
-    img = np.expand_dims(img,0)
-    return img
+def predict_skin_cancer(model,data):
 
+    prob=model.predict(data)[0][0]
 
-def predict(model,img):
+    label="Malignant" if prob>0.5 else "Benign"
+    conf=prob*100 if label=="Malignant" else (1-prob)*100
 
-    p = model.predict(img)[0][0]
+    return label,conf,prob
 
-    if p > 0.5:
-        return "Malignant", p*100, p
-    else:
-        return "Benign", (1-p)*100, p
+def risk_level(conf):
+
+    if conf>80:
+        return "High"
+    elif conf>60:
+        return "Medium"
+    return "Low"
