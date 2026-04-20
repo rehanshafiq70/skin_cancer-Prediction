d74bfc41d90caae87e9a319a22dd292e9e210521
Rehan Shafique <rehanshafiq6540@gmail.com>
2026-04-20 14:15:14 +0500

    Update and rename theme.py to style.py

---
+++ style.py
@@ -0,0 +1,24 @@
+def load_global_style():
+
+    return """
+<style>
+
+body{
+background:#0b0f14;
+color:white;
+}
+
+.stButton>button{
+background:#00ff9c;
+border-radius:12px;
+font-weight:bold;
+}
+
+.stMetric{
+background:#111;
+padding:10px;
+border-radius:10px;
+}
+
+</style>
+"""
