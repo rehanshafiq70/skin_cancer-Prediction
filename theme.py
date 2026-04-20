df90cbea65310b312ee51cf54490ec19788c951f
Rehan Shafique <rehanshafiq6540@gmail.com>
2026-04-20 13:58:59 +0500

    Create theme.py

---
+++ theme.py
@@ -0,0 +1,10 @@
+def load_theme():
+    return """
+    <style>
+    body{background:#0b0f14;color:white;}
+    .stButton>button{
+        background:#00ff9c;
+        border-radius:12px;
+    }
+    </style>
+    """
